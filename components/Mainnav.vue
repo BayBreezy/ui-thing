15b29c40262c99f3a7b3d1671c82342e2412a2fe
BayBreezy <behon.baker@yahoo.com>
2023-09-08 01:08:29 -0500

    chore: add theme switcher in nav

--- components/Mainnav.vue
+++ components/Mainnav.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="sticky top-0 z-20 border-b bg-background/80 backdrop-blur">
-    <div class="container flex h-14 items-center">
+    <div class="container flex h-14 items-center justify-between">
       <div class="flex items-center gap-10">
         <NuxtLink to="/" class="text-lg font-bold">UI Thing</NuxtLink>
         <nav class="flex items-center space-x-6 text-sm font-medium">
@@ -26,8 +26,44 @@
           </UIDropdownMenu>
         </nav>
       </div>
+
+      <div class="flex items-center gap-5">
+        <UIDropdownMenu>
+          <UIDropdownMenuTrigger as-child>
+            <UIButton class="h-9 w-9" variant="ghost" size="icon"
+              ><Icon :name="currentIcon || 'lucide:sun'" class="h-[18px] w-[18px]"
+            /></UIButton>
+          </UIDropdownMenuTrigger>
+          <UIDropdownMenuContent align="end" :side-offset="5">
+            <UIDropdownMenuItem
+              class="cursor-pointer"
+              v-for="(m, i) in modes"
+              :key="i"
+              :icon="m.icon"
+              :title="m.title"
+              @click="setTheme(m.value)"
+            />
+          </UIDropdownMenuContent>
+        </UIDropdownMenu>
+      </div>
     </div>
   </header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const mode = useColorMode();
+
+  const modes = [
+    { icon: "lucide:sun", title: "Light", value: "light" },
+    { icon: "lucide:moon", title: "Dark", value: "dark" },
+    { icon: "lucide:laptop", title: "System", value: "auto" },
+  ];
+
+  const setTheme = (val: string) => {
+    mode.value = val as "light" | "dark" | "auto";
+  };
+
+  const currentIcon = computed(() => {
+    return modes.find((m) => m.value === mode.value)?.icon;
+  });
+</script>
