d0a91b60863d029d5bfee2ad269617756185d723
Behon Baker <behon.baker@yahoo.com>
2023-10-31 17:45:25 -0500

    docs: update next and prev components

--- components/Docs/Footer.vue
+++ components/Docs/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="not-prose mt-10 py-10">
-    <div class="flex gap-5 md:items-center md:justify-between">
+    <div class="grid grid-cols-1 gap-5 md:grid-cols-2 md:items-center md:justify-between">
       <NuxtLink
         :to="prev._path"
-        class="flex gap-4 rounded-lg border p-5 transition hover:border-primary md:basis-1/2"
+        class="flex basis-full gap-4 rounded-lg border p-5 transition hover:border-primary md:basis-1/2"
         v-if="prev"
       >
         <div class="flex h-10 w-10 shrink-0 items-center justify-center rounded-full border">
@@ -18,7 +18,7 @@
       </NuxtLink>
       <NuxtLink
         :to="next._path"
-        class="flex gap-4 rounded-lg border p-5 transition hover:border-primary md:basis-1/2"
+        class="flex basis-full gap-4 rounded-lg border p-5 transition hover:border-primary md:basis-1/2"
         v-if="next"
       >
         <div class="flex flex-col gap-1">
