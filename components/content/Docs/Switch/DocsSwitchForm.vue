a905a5f339cc1380b6a70cd9aef36bbf895b3154
BayBreezy <behon.baker@yahoo.com>
2023-11-01 09:08:54 -0500

    chore: add form example for switch

--- components/content/Docs/Switch/DocsSwitchForm.vue
+++ components/content/Docs/Switch/DocsSwitchForm.vue
@@ -1,53 +1,3 @@
----
-title: Switch
-description: A control that allows the user to toggle between checked and not checked.
-links:
-  - title: Radix-Vue
-    href: https://www.radix-vue.com/components/switch.html
-    icon: "simple-icons:radixui"
-  - title: API Reference
-    href: https://www.radix-vue.com/components/switch.html#api-reference
-    icon: "icon-park-solid:api"
----
-
-## Source code
-
-Click :SourceCodeLink{component="Switch"} to see the source code for this component on GitHub. Feel free to copy it and adjust it for your own use.
-
-## Installation
-
-```bash
-npx ui-thing@latest add switch
-```
-
-## Usage
-
-### Basic example
-
-::ShowCase{component="DocsSwitch"}
-
-#code
-
-```vue [DocsSwitch.vue]
-<template>
-  <div class="flex w-full justify-center">
-    <div class="flex items-center space-x-2">
-      <UISwitch id="airplane-mode" />
-      <UILabel for="airplane-mode">Airplane Mode</UILabel>
-    </div>
-  </div>
-</template>
-```
-
-::
-
-### Form
-
-::ShowCase{component="DocsSwitchForm"}
-
-#code
-
-```vue [DocsSwitchForm.vue]
 <template>
   <form @submit="onSubmit" class="flex w-full justify-center">
     <div class="w-full">
@@ -86,6 +36,3 @@ npx ui-thing@latest add switch
     });
   });
 </script>
-```
-
-::
