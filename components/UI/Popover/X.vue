367094bc00005185688039018748418d5a85b671
Behon Baker <behon.baker@yahoo.com>
2023-11-14 21:01:35 -0500

    chore(popover): update prop forwarding

--- components/UI/Popover/X.vue
+++ components/UI/Popover/X.vue
@@ -29,5 +29,5 @@
     base: "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground",
   });
 
-  const forwarded = useForwardProps(useOmit(props, ["class", "icon", "srText"]));
+  const forwarded = useForwardProps(props);
 </script>
