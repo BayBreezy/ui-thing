d2cfde197f43fe38ba4b627734114eb6fb39f872
Behon Baker <behon.baker@yahoo.com>
2023-11-14 20:20:00 -0500

    chore(label): update prop forwarding

--- components/UI/Label.vue
+++ components/UI/Label.vue
@@ -1,11 +1,11 @@
 <template>
-  <Label :class="styles({ class: props.class })" v-bind="forwarded">
+  <Label :class="styles({ class: props.class })" v-bind="props">
     <slot />
   </Label>
 </template>
 
 <script lang="ts" setup>
-  import { Label, useForwardProps } from "radix-vue";
+  import { Label } from "radix-vue";
   import type { LabelProps } from "radix-vue";
 
   const props = defineProps<
@@ -13,7 +13,6 @@
       class?: any;
     }
   >();
-  const forwarded = useForwardProps(useOmit(props, ["class"]));
 
   const styles = tv({
     base: "inline-block text-base font-medium leading-none hover:cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70 sm:text-sm",
