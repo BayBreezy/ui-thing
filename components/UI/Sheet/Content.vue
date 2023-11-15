3621dadc9d797e4a03aeaceb61d36f438d4d3427
Behon Baker <behon.baker@yahoo.com>
2023-11-14 21:07:23 -0500

    chore(sheet): update prop forwarding

--- components/UI/Sheet/Content.vue
+++ components/UI/Sheet/Content.vue
@@ -43,7 +43,7 @@
     }
   >();
   const emits = defineEmits<DialogContentEmits>();
-  const forwarded = useForwardPropsEmits(useOmit(props, ["title", "class"]), emits);
+  const forwarded = useForwardPropsEmits(props, emits);
 
   const styles = tv({
     base: "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
