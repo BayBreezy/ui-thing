2e30f42054462ea7fa0c1c2a9072b12d6386f0a3
Behon Baker <behon.baker@yahoo.com>
2023-11-14 21:04:26 -0500

    chore(select): update styles

--- components/UI/Select/Item.vue
+++ components/UI/Select/Item.vue
@@ -22,6 +22,6 @@
   >();
 
   const styles = tv({
-    base: "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+    base: "relative flex w-full cursor-pointer select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
   });
 </script>
