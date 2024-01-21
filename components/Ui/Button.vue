b49aa19f18e168f3c15ea55cf5dbce93061d3417
BayBreezy <behon.baker@yahoo.com>
2024-01-21 08:43:37 -0500

    chore: add text prop to button

--- components/Ui/Button.vue
+++ components/Ui/Button.vue
@@ -15,7 +15,7 @@
     :type="type"
     @click="onClick"
   >
-    <slot></slot>
+    <slot>{{ text }}</slot>
   </component>
 </template>
 
@@ -25,16 +25,28 @@
   type ButtonProps = VariantProps<typeof buttonStyles>;
   const props = withDefaults(
     defineProps<{
+      /** The type fro the button */
       type?: "button" | "submit" | "reset";
+      /** Whether the button is disabled */
       disabled?: boolean;
+      /** Whether the button is loading */
       loading?: boolean;
+      /** The action to perform when the button is clicked */
       onClick?: any;
+      /** The location to navigate to when the button is clicked */
       to?: string | RouteLocationRaw;
+      /** The location to navigate to when the button is clicked */
       href?: string;
+      /** The element to render the button as */
       as?: string;
+      /** Custom class(es) to add to parent element */
       class?: any;
+      /** The variant of the button */
       variant?: ButtonProps["variant"];
+      /** The size of the button */
       size?: ButtonProps["size"];
+      /** The text to display in the button */
+      text?: string;
     }>(),
     {
       type: "button",
