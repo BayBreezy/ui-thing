d5966b119cc80e5fa15da0874d802304d155ea0c
Behon Baker <behon.baker@yahoo.com>
2024-04-21 18:40:11 -0500

    feat(blocks): add social proof section

---
+++ components/content/Block/SocialProof/BlockSocialProof1.vue
@@ -0,0 +1,22 @@
+<template>
+  <UiContainer class="py-16 lg:py-24">
+    <p class="mb-8 text-center text-muted-foreground">Join 4,000+ companies already growing</p>
+    <div class="flex flex-wrap items-center justify-center gap-5">
+      <template v-for="n in companies" :key="n.text">
+        <div>
+          <span class="sr-only">{{ n.text }}</span>
+          <Icon :name="n.icon" class="h-24 w-24" />
+        </div>
+      </template>
+    </div>
+  </UiContainer>
+</template>
+
+<script lang="ts" setup>
+  const companies = [
+    { text: "Company One", icon: "logos:100tb" },
+    { text: "Company Two", icon: "logos:aha" },
+    { text: "Company Three", icon: "logos:airbnb" },
+    { text: "Company Four", icon: "logos:akamai" },
+  ];
+</script>
---
+++ content/5.blocks/socialproof.md
@@ -0,0 +1,12 @@
+---
+title: Social Proof
+description: On this page you will find different ways in which you can style the Social Proof sections of your website.
+---
+
+## Note
+
+In order to see the full(Desktop) view, please click on the `External View` button ( :icon{name="lucide:square-arrow-out-up-right"} ) at the top of the style.
+
+## Style One
+
+:BlockShowcase{blockPath="SocialProof/BlockSocialProof1" component="BlockSocialProof1" iframeHeight="300px" }
