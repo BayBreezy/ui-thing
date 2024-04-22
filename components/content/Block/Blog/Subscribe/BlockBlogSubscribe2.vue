21f695281cb89a82c9447de1719e7a0e72a2279a
Behon Baker <behon.baker@yahoo.com>
2024-04-21 19:02:20 -0500

    feat(blocks): add blog subscribe cards

---
+++ components/content/Block/Blog/Subscribe/BlockBlogSubscribe2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <UiCard class="rounded-none border-t-4 border-t-primary">
+      <UiCardContent>
+        <div
+          class="inline-flex h-14 w-14 items-center justify-center rounded-md border bg-background shadow-sm"
+        >
+          <Icon name="lucide:mail" class="h-6 w-6" />
+        </div>
+
+        <h3 class="mb-2 mt-8 text-2xl font-semibold">Weekly newsletter</h3>
+        <p class="mb-8 text-muted-foreground">
+          No spam. Just the latest releases and tips, interesting articles, and exclusive interviews
+          in your inbox every week.
+        </p>
+        <form>
+          <div class="w-full">
+            <UiVeeInput required placeholder="Enter your email" aria-label="Enter your email" />
+          </div>
+          <p class="my-4 text-sm text-muted-foreground">
+            Read about our privacy policy <a href="#" class="text-primary">here</a>.
+          </p>
+          <div>
+            <UiButton size="lg" class="w-full whitespace-nowrap" type="submit">Subscribe</UiButton>
+          </div>
+        </form>
+      </UiCardContent>
+    </UiCard>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
---
+++ content/5.blocks/blogsubscribe.md
@@ -0,0 +1,16 @@
+---
+title: Blog - Subscribe
+description: On this page you will find different ways in which you can style the subscribe card that can be in your blogs on your website.
+---
+
+## Note
+
+In order to see the full(Desktop) view, please click on the `External View` button ( :icon{name="lucide:square-arrow-out-up-right"} ) at the top of the style.
+
+## Style One
+
+:BlockShowcase{blockPath="Blog/Subscribe/BlockBlogSubscribe1" component="BlockBlogSubscribe1" containerClass="container py-10" iframeHeight="550px"}
+
+## Style Two
+
+:BlockShowcase{blockPath="Blog/Subscribe/BlockBlogSubscribe2" component="BlockBlogSubscribe2" containerClass="container py-10" iframeHeight="550px"}
