3c27237e66eea319f22a067830e5c661746381f6
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-06-04 23:41:13 -0500

    chore: add form input class to input element

--- components/Ui/Input.vue
+++ components/Ui/Input.vue
@@ -1,5 +1,5 @@
 <template>
-  <input v-bind="props" v-model="localModel" :class="styles({ class: props.class })" >
+  <input v-bind="props" v-model="localModel" :class="styles({ class: props.class })" />
 </template>
 
 <script lang="ts" setup>
@@ -22,6 +22,6 @@
   const localModel = useVModel(props, "modelValue", emits);
 
   const styles = tv({
-    base: "h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-[16px] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground file:hover:cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+    base: "form-input h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:px-1 file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground file:hover:cursor-pointer focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
   });
 </script>
