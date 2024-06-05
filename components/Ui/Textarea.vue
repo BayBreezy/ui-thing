77deb7f9118ecabc1788177b077629a653535d86
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-06-04 23:53:59 -0500

    chore: add form textarea class to text area component

--- components/Ui/Textarea.vue
+++ components/Ui/Textarea.vue
@@ -21,6 +21,6 @@
   const localModel = useVModel(props, "modelValue", emits);
 
   const styles = tv({
-    base: "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+    base: "form-textarea flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
   });
 </script>
