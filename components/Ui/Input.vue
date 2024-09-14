c878c5f7e4c705cf66e713cb46d518a6cd4e5bb3
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-09-13 19:55:02 -0500

    chore: make input show correct date icon color in dark mode

--- components/Ui/Input.vue
+++ components/Ui/Input.vue
@@ -23,6 +23,6 @@
   const localModel = useVModel(props, "modelValue", emits);
 
   const styles = tv({
-    base: "form-input h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:px-1 file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground file:hover:cursor-pointer focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+    base: "form-input h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:px-1 file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground file:hover:cursor-pointer focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:[color-scheme:dark] sm:text-sm",
   });
 </script>
