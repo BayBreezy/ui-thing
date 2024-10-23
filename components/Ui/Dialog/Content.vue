b26168af85836aa712e2cee9ace7890ae8c8f745
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-10-23 06:52:35 -0500

    chore: update style for close button in dialog content. closes Dialog close button offset #23

--- components/Ui/Dialog/Content.vue
+++ components/Ui/Dialog/Content.vue
@@ -21,9 +21,9 @@
       </slot>
       <UiDialogClose
         v-if="!hideClose"
-        class="absolute right-4 top-2 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+        class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
       >
-        <Icon name="lucide:x" class="h-4 w-4" />
+        <Icon name="lucide:x" class="h-4 w-4" mode="svg" />
         <span class="sr-only">Close</span>
       </UiDialogClose>
     </DialogContent>
