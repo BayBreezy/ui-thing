7d9927057dc70f0cdd6aedeca50ef178c42f3ed0
Behon Baker <behon.baker@yahoo.com>
2024-12-27 11:19:33 -0500

    feat: add `maxlength` & `pattern` props to textarea
    Thanks @arshx86

--- app/components/Ui/Textarea.vue
+++ app/components/Ui/Textarea.vue
@@ -2,25 +2,33 @@
   <textarea
     v-bind="props"
     :value="modelValue"
-    @input="handleInput"
     :class="styles({ class: props.class })"
+    @input="handleInput"
   />
 </template>
 
 <script lang="ts" setup>
-  import { computed } from "vue";
-
   const props = withDefaults(
     defineProps<{
+      /** Additional classes to add to the textarea */
       class?: any;
+      /** The name of the textarea */
       name?: string;
+      /** The id of the textarea */
       id?: string;
+      /** The placeholder of the textarea */
       placeholder?: string;
+      /** Whether the textarea is required */
       required?: boolean;
+      /** Whether the textarea is disabled */
       disabled?: boolean;
+      /** The number of rows to display */
       rows?: number;
+      /** The value of the textarea */
       modelValue?: string;
+      /** The maximum number of characters allowed */
       maxlength?: number;
+      /** The `RegExp` pattern of the textarea */
       pattern?: string;
     }>(),
     {
@@ -54,6 +62,6 @@
   };
 
   const styles = tv({
-    base: "form-textarea flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 sm:text-sm",
+    base: "form-textarea flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:[color-scheme:dark] sm:text-sm",
   });
 </script>
