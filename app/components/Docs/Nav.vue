dab419ca3107e0fbcce7d54f0cc8ef237a053d36
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-10-30 01:16:30 -0400

    feat: refactor Nav component. Removed client-only wrapper

--- app/components/Docs/Nav.vue
+++ app/components/Docs/Nav.vue
@@ -20,47 +20,45 @@
           >{{ l.label }}</UiBadge
         >
       </div>
-      <ClientOnly>
-        <UiCollapsible
-          v-if="l.children"
-          v-slot="{ open }"
-          :default-open="route.path.includes(l.path)"
-          class="flex flex-col gap-4"
+      <UiCollapsible
+        v-if="l.children"
+        v-slot="{ open }"
+        :default-open="route.path.includes(l.path)"
+        class="flex flex-col gap-4"
+      >
+        <UiCollapsibleTrigger
+          class="flex items-center justify-between rounded-sm px-3 py-2 hover:bg-accent"
+          :class="[
+            route.path.startsWith(l.path)
+              ? 'bg-accent text-accent-foreground dark:bg-accent/50'
+              : '',
+          ]"
         >
-          <UiCollapsibleTrigger
-            class="flex items-center justify-between rounded-sm px-3 py-2 hover:bg-accent"
-            :class="[
-              route.path.startsWith(l.path)
-                ? 'bg-accent text-accent-foreground dark:bg-accent/50'
-                : '',
-            ]"
-          >
-            <div class="flex items-center gap-2.5">
-              <Icon v-if="l.icon" :name="l.icon" class="size-4 text-muted-foreground" />
-              <p class="text-[17px] font-medium sm:text-sm">{{ l.title }}</p>
-              <UiBadge
-                v-if="l.label"
-                class="rounded-md px-2 py-0 text-xs md:text-[10px] dark:bg-lime-500"
-                >{{ l.label }}</UiBadge
-              >
-            </div>
-            <Icon
-              name="lucide:chevron-down"
-              :class="[
-                'shrink-0 transform text-muted-foreground transition',
-                open ? 'rotate-180' : 'rotate-0',
-              ]"
-            />
-          </UiCollapsibleTrigger>
-          <UiCollapsibleContent class="pl-5">
-            <div
-              class="relative pt-1 pl-6 before:absolute before:left-0 before:h-[calc(100%-23px)] before:w-px before:bg-border dark:before:bg-accent"
+          <div class="flex items-center gap-2.5">
+            <Icon v-if="l.icon" :name="l.icon" class="size-4 text-muted-foreground" />
+            <p class="text-[17px] font-medium sm:text-sm">{{ l.title }}</p>
+            <UiBadge
+              v-if="l.label"
+              class="rounded-md px-2 py-0 text-xs md:text-[10px] dark:bg-lime-500"
+              >{{ l.label }}</UiBadge
             >
-              <DocsNav class="gap-4" :links="l.children" />
-            </div>
-          </UiCollapsibleContent>
-        </UiCollapsible>
-      </ClientOnly>
+          </div>
+          <Icon
+            name="lucide:chevron-down"
+            :class="[
+              'shrink-0 transform text-muted-foreground transition',
+              open ? 'rotate-180' : 'rotate-0',
+            ]"
+          />
+        </UiCollapsibleTrigger>
+        <UiCollapsibleContent class="pl-5">
+          <div
+            class="relative pt-1 pl-6 before:absolute before:left-0 before:h-[calc(100%-23px)] before:w-px before:bg-border dark:before:bg-accent"
+          >
+            <DocsNav class="gap-4" :links="l.children" />
+          </div>
+        </UiCollapsibleContent>
+      </UiCollapsible>
     </template>
   </nav>
 </template>
