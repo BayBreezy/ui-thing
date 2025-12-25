82198378675db3e3b09286de3a65372dde7eec1b
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-12-25 05:42:04 -0500

    chore(components-api): add search functionality to get component api endpoint

--- server/api/components/index.get.ts
+++ server/api/components/index.get.ts
@@ -1,5 +1,23 @@
-import comps from "../../utils/comp";
+import fuse from "fuse.js";
+import { z } from "zod";
 
-export default defineEventHandler(() => {
-  return comps;
+export default defineEventHandler(async (event) => {
+  // accept a search param
+  const { search } = await getValidatedQuery(
+    event,
+    z.object({
+      search: z.coerce.string().trim().nullish(),
+    }).parse
+  );
+  // if no search term was provided, return all components
+  if (!search) {
+    return comp;
+  }
+  // search for the component using fuse.js
+  const fuseInstance = new fuse(comp, {
+    keys: ["name", "value", "docsPath"],
+    threshold: 0.3,
+  });
+  const results = fuseInstance.search(search);
+  return results.map((r) => r.item);
 });
