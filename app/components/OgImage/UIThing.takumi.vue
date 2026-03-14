32498b33b89646b4a7cca325ab44faf903c01258
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-03-14 04:49:59 -0500

    chore: update the style for og-image

--- app/components/OgImage/UIThing.takumi.vue
+++ app/components/OgImage/UIThing.takumi.vue
@@ -1,13 +1,19 @@
 <template>
   <div
-    class="h-full w-full bg-cover bg-center"
-    :style="{ backgroundImage: 'url(https://ui-thing.behonbaker.com/cover-blank.png)' }"
+    class="size-full bg-cover bg-center"
+    :style="{ backgroundImage: 'url(https://uithing.com/cover-blank.png)' }"
   >
-    <div class="flex h-full w-full flex-col items-center justify-center">
-      <h1 class="text-center text-8xl font-bold tracking-tight text-white capitalize">
+    <div class="flex size-full flex-col items-center justify-center">
+      <div
+        class="mb-10 inline-flex items-center gap-2 rounded-full border border-solid border-zinc-700/50 px-4 py-2 text-center text-2xl font-bold text-white"
+      >
+        <img src="https://uithing.com/icon.png" alt="UI Thing Logo" class="size-7 rounded-lg" />
+        UI <span class="text-rose-500">Thing</span>
+      </div>
+      <h1 class="mb-10 text-center text-7xl font-bold tracking-tight text-white capitalize">
         {{ title }}
       </h1>
-      <p class="mx-auto max-w-6xl text-center text-4xl leading-relaxed text-white">
+      <p class="mx-auto max-w-5xl text-center text-4xl leading-[1.8] text-white">
         {{ description }}
       </p>
     </div>
@@ -17,7 +23,7 @@
 <script lang="ts" setup>
   defineOptions({ inheritAttrs: false });
   defineProps<{
-    title: string;
-    description: string;
+    title?: string;
+    description?: string;
   }>();
 </script>
