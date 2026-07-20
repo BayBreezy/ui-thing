1cc670882c6ab09f90ded2c807a759bb4857d2d3
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-07-19 18:22:46 -0700

    chore: bump deps & update page maxWidth

--- app/app.config.ts
+++ app/app.config.ts
@@ -18,6 +18,9 @@ export default defineAppConfig({
           favicon: "/favicon.ico",
         },
       },
+      body: {
+        maxWidth: "800px",
+      },
       extraLinks: [
         { icon: "lucide:star", label: "Star on Github", external: true, href: repoBase },
         {
