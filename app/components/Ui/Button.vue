ad016e144081362ea842408d70a194d39fac1911
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-08-21 22:52:56 -0700

    fix: manually define nuxtlink props to pass to button

--- app/components/Ui/Button.vue
+++ app/components/Ui/Button.vue
@@ -45,8 +45,50 @@
   import { useForwardProps } from "reka-ui";
   import { normalizeClass } from "vue";
   import type { HtmlHTMLAttributes } from "vue";
+  import type { RouteLocationRaw } from "vue-router";
 
-  import type { NuxtLinkProps } from "#app/components";
+  type NuxtLinkCustomProps = {
+    /** Route Location the link should navigate to when clicked on. */
+    to?: RouteLocationRaw;
+    /** An alias for `to`. If used with `to`, `href` will be ignored. */
+    href?: string;
+    /**
+     * Forces the link to be considered as external (true) or internal (false). This is helpful to
+     * handle edge-cases.
+     */
+    external?: boolean;
+    /** Where to display the linked URL, as the name for a browsing context. */
+    target?: "_blank" | "_parent" | "_self" | "_top" | (string & {}) | null;
+    /**
+     * A rel attribute value to apply on the link. Defaults to "noopener noreferrer" for external
+     * links.
+     */
+    rel?: "noopener" | "noreferrer" | "nofollow" | "sponsored" | "ugc" | (string & {}) | null;
+    /** If set to true, no rel attribute will be added to the link. */
+    noRel?: boolean;
+    /** A class to apply to links that have been prefetched. */
+    prefetchedClass?: string;
+    /** When enabled will prefetch middleware, layouts and payloads of links in the viewport. */
+    prefetch?: boolean;
+    /**
+     * Allows controlling when to prefetch links. By default, prefetch is triggered only on
+     * visibility.
+     */
+    prefetchOn?:
+      | "visibility"
+      | "interaction"
+      | Partial<{
+          visibility: boolean;
+          interaction: boolean;
+        }>;
+    /** Escape hatch to disable `prefetch` attribute. */
+    noPrefetch?: boolean;
+    /**
+     * An option to either add or remove trailing slashes in the `href` for this specific link.
+     * Overrides the global `trailingSlash` option if provided.
+     */
+    trailingSlash?: "append" | "remove";
+  };
 
   /** Exported button styles that can be used by other components. */
   export const buttonStyles = tv({
@@ -108,7 +150,7 @@
     },
   });
   export type ButtonVariants = VariantProps<typeof buttonStyles>;
-  export type ButtonProps = NuxtLinkProps & {
+  export type ButtonProps = Partial<NuxtLinkCustomProps> & {
     /** The type for the button. */
     type?: "button" | "submit" | "reset";
     /** Whether the button is disabled. */
